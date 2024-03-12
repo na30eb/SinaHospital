e5e2774f17629a6eeb148813726f48bf175681b9
nastaran_ebrahimi <r.sulfus2@gmail.com>
2024-03-12 14:50:46 +0330

    modifying sign up page + adding sign up action php

---
+++ Signup.php
@@ -0,0 +1,162 @@
+<html lang="en" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.rtl.min.css">
+    <script src="Scripts/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/Style.css" />
+    <link rel="stylesheet" href="css/Style1.css" />
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" />
+    <Style>
+        @media (min-width:1024px) {
+            body {
+                background-image: url('img/Group\ 33995.png');
+                background-repeat: no-repeat;
+                background-position: left;
+            }
+        }
+    </Style>
+
+
+    <title>بیمارستان تخصصی سینا|صفحه ثبت نام</title>
+</head>
+</head>
+
+<body>
+    <div class="BackBtn"><img class="BackBtn" src="img/Group 33999.png"
+            onclick="window.location.href=Index.html"></div>
+    <div class="container">
+
+
+
+
+
+
+
+
+        <div class="user">
+            <div class="p-2 mx-auto mx-md-0"><img src="img/logo-copy64-1.png" class="LoginLogo LoginLogo222"></div>
+            <div class="p-2 mx-auto mx-md-0"><img src="img/Group 1.png" class="LoginLogo2 LoginLogo22"></div>
+            <div class="p-2">
+                <h2 class="h2Login h2Login2">ثبت نام</h2>
+            </div>
+
+
+            <div>
+                <form class="row g-2 user" action="SignupAction.php" method="post">
+                    <div class="row cols-12 col-xs-12">
+                        <div class="col-md-4 col-lg-3">
+                            <label class="form-label form-label2">نام </label>
+
+
+                            <input type="text" class="form-control inputSignup " name="fname">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                className="w-6 h-6" class="inputicon inputicon2">
+                                <path fillRule="evenodd"
+                                    d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                                    clipRule="evenodd" />
+                            </svg>
+
+
+                        </div>
+
+                        <div class="col-md-4 col-xs-12 col-lg-3">
+                            <label class="form-label form-label2">نام خانوادگی</label>
+                            <input type="text" class="form-control inputSignup" name="lname">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                className="w-6 h-6" class="inputicon inputicon2">
+                                <path fillRule="evenodd"
+                                    d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                                    clipRule="evenodd" />
+                            </svg>
+
+                        </div>
+                    </div>
+
+
+
+                    <div class="row cols-12 col-xs-12 ">
+                        <div class="col-md-4 col-lg-3">
+                            <label class="form-label form-label2">شماره موبایل </label>
+                            <input type="tel" class="form-control inputSignup" name="phonenum"> 
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                class="w-6 h-6 inputicon inputicon2">
+                                <path fill-rule="evenodd"
+                                    d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+
+
+                        </div>
+
+                        <div class="col-md-4 col-lg-3" col-xs-12>
+                            <label class="form-label form-label2">ایمیل </label>
+                            <input type="email" class="form-control inputSignup" name="email">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                class="w-6 h-6 inputicon inputicon2">
+                                <path
+                                    d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+                                <path
+                                    d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+                            </svg>
+
+
+                        </div>
+                    </div>
+
+                    <div class="row cols-12 col-xs-12">
+
+                        <div class="col-md-4 col-lg-3">
+                            <label class="form-label form-label2">رمز عبور </label>
+                            <input type="password" class="form-control   inputSignup" dir="ltr" name="passwrd">
+                            <img src="img/Vector.svg" class="position-absolute span--1 inputicon inputicon2" alt="">
+
+                        </div>
+
+                        <div class="col-md-4 col-xs-12">
+                            <label class="form-label form-label2">تکرار رمز عبور </label>
+                            <input type="password" class="form-control  inputSignup"  dir="ltr" name="repasswrd">
+                            <img src="img/Vector.svg" class="position-absolute span--1 inputicon inputicon2" alt="">
+
+                        </div>
+
+                    </div>
+
+                    <div class="row cols-12">
+                        <button type="submit" class="btn btn-primary BtnSignup2 ">ثبت نام</button>
+
+                    </div>
+
+                    <div class="col-md-2 col-xs-12">
+                        <label class="form-label form-label2">فراموشی رمز عبور </label>
+                    </div>
+
+
+                </form>
+
+
+
+
+
+
+
+
+            </div>
+        </div>
+
+
+
+
+
+
+
+    </div>
+
+
+
+    </div>
+
+</body>
+
+</html>
